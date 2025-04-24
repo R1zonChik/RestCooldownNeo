82405c551425854bbc2c27c5455c8081cce00588
Maxim <maxim.fakt@yandex.ru>
2025-04-24 22:05:02 +0400

    фикс 3

--- src/main/java/ru/refontstudio/restcooldownneo/commands/CancelCommand.java
+++ src/main/java/ru/refontstudio/restcooldownneo/commands/CancelCommand.java
@@ -62,14 +62,10 @@ public class CancelCommand implements CommandExecutor, TabCompleter {
                 // Если это hub - пропускаем эту проверку и продолжаем выполнение
             }
 
+            // Для команды отмены телепортации - просто вызываем метод без дополнительных сообщений
             if (label.equalsIgnoreCase("neocommand_cancel")) {
-                // УДАЛЯЕМ ДУБЛИРОВАНИЕ: метод cancelAllCommands уже отправляет сообщение
+                // Метод cancelAllCommands уже сам отправляет сообщение
                 this.cooldownLogic.cancelAllCommands(player);
-                // Убираем этот блок кода, который дублирует сообщение:
-                // if (cancelled) {
-                //     group = this.plugin.getConfig().getString("messages.teleport-is-cancel", "");
-                //     player.sendMessage(ChatColor.translateAlternateColorCodes('&', group));
-                // }
                 return true;
             } else {
                 String newCommand;
@@ -83,7 +79,7 @@ public class CancelCommand implements CommandExecutor, TabCompleter {
                         player.sendMessage(ChatColor.translateAlternateColorCodes('&', newCommand));
                         return true;
                     } else if (args[0].equalsIgnoreCase("info")) {
-                        newCommand = "&f\n&c• &cRestCooldownNeo &fMade By &5Refont &f| Version: &c1.0\n&f\n  &fДоступные команды:\n&c• &c/neocooldown info &f- Просмотреть информацию о плагине\n&c• &c/neocooldown reload - &fПерезагрузить плагин\n&f\n&f  Доступные Пермишены:\n&c• &cneocooldown.admin &f- Права на все команды плагина +пропуск кулдауна\n&f";
+                        newCommand = "&f\\n&c• &cRestCooldownNeo &fMade By &5Refont &f| Version: &c1.0\\n&f\\n  &fДоступные команды:\\n&c• &c/neocooldown info &f- Просмотреть информацию о плагине\\n&c• &c/neocooldown reload - &fПерезагрузить плагин\\n&f\\n&f  Доступные Пермишены:\\n&c• &cneocooldown.admin &f- Права на все команды плагина +пропуск кулдауна\\n&f";
                         player.sendMessage(ChatColor.translateAlternateColorCodes('&', newCommand));
                         return true;
                     } else if (args[0].equalsIgnoreCase("reload")) {
@@ -97,7 +93,7 @@ public class CancelCommand implements CommandExecutor, TabCompleter {
                             return true;
                         } else {
                             newCommand = args[1];
-                            group = args[2];
+                            String group = args[2]; // Здесь переменная group определена правильно
                             FileConfiguration config = this.plugin.getConfig();
                             if (config.isConfigurationSection("command-groups." + group)) {
                                 List<String> commandsCooldown = config.getStringList("command-groups." + group + ".commands-cooldown");
@@ -109,7 +105,6 @@ public class CancelCommand implements CommandExecutor, TabCompleter {
                             } else {
                                 player.sendMessage(ChatColor.RED + "Группа " + group + " Не найдена.");
                             }
-
                             return true;
                         }
                     } else {
